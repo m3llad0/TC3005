605df8f06ca8bf290beffb623bfc00ee93536d28
m3llad0 <a01655451@tec.mx>
2023-04-18 13:33:20 -0600

    created user controller routes

--- back-end/ts-project/src/controllers/userController.ts
+++ back-end/ts-project/src/controllers/userController.ts
@@ -1,5 +1,37 @@
+import { Request, Response } from "express";
 import AbstractController from "./abstractController";
 
 class UserController extends AbstractController{
+    protected validateBody(type: any) {
+        throw new Error("Method not implemented.");
+    }
+
+    /*Singleton */
+        /*Atributo de clase*/
+    private static instance: UserController;
+        /*Metodo de clase*/
+    public static getInstance(): AbstractController{
+        if(this.instance){
+            return this.instance;
+        }
+
+        this.instance = new UserController('user');
+        return this.instance;
+    }
+
+    protected initRoutes(): void{
+        this.router.get('/readUser', this.getReadUsers.bind(this));
+        this.router.post('/newUser', )
+
+    }
+
+    private getReadUsers(req: Request, res: Response){
+        res.status(200).send({'message': 'Servicio de lectura de usuarios'});
+    }
     
+    private postNewUser(req: Request, res: Response){
+        res.status(200).send({'message': 'Created new user!'})
+    }
 }
+
+export default UserController;
