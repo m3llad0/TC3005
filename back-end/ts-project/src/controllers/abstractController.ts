34e0ca01e9cb92e9134ffc0624195dddf107da73
m3llad0 <a01655451@tec.mx>
2023-04-18 13:33:08 -0600

    added missing init routes

--- back-end/ts-project/src/controllers/abstractController.ts
+++ back-end/ts-project/src/controllers/abstractController.ts
@@ -16,6 +16,7 @@ export default abstract class AbstractController {
     }
     protected constructor(prefix: string) {
         this._prefix = prefix;
+        this.initRoutes();
     }
 
     /*Initialiaze routes*/
