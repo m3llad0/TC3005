bf3f31167213ff6b3a1c7223702549a667caa06d
m3llad0 <a01655451@tec.mx>
2023-04-24 15:18:52 -0600

    env variables

---
+++ back-end/ts-project/src/config/index.ts
@@ -0,0 +1,5 @@
+export const PORT:number = process.env.PORT ? +process.env.PORT: 8000;
+export const NODE_ENV: string = process.env.NODE_ENV ? process.env.NODE_ENV: 'development';
+export const DB_USER: string = process.env.DB_NAME ? process.env.DB_NAME: 'root';
+export const DB_PASSWORD: string = process.env.DB_PASSWORD ? process.env.DB_PASSWORD: 'password'
+export const DB_HOST: string = process.env.DB_HOST ? process.env.DB_HOST: 'localhost'
